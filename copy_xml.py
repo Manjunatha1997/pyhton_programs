202d00c1c2a415436ea4877ad99a53215b698e0e
Manjunatha1997 <lovelymanju198@gmail.com>
2021-02-12 13:17:22 +0530

    upadted copy xml file added

--- copy_xml.py
+++ copy_xml.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import xml.etree.ElementTree as ET
-
+import cv2
 
 inp_path = "/home/manju/Desktop/test/"
 
@@ -24,6 +24,7 @@ path = inp_path
 files = os.listdir(path)
 
 for file in files:
+
 	if file.endswith('.xml'):
 		
 		tree = ET.parse(path+file)
@@ -37,5 +38,18 @@ for file in files:
 
 			if elt.tag == 'path':
 				elt.text = (path+file.split('.')[0]+'.jpg')
+
+			if elt.tag == 'width':
+				img = (path+file.split('.')[0]+'.jpg')
+				img = cv2.imread(img)
+				h,w,c = img.shape
+				elt.text = str(w)
+			if elt.tag == 'height':
+				img = (path+file.split('.')[0]+'.jpg')
+				img = cv2.imread(img)
+				h,w,c = img.shape
+				elt.text = str(h)
+			
+
 
 		tree.write(path+file)
